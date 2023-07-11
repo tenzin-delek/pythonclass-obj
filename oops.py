bb690d921e7f71434f60485dd21fbbc96d41da67
tenzin-delek <85842889+tenzin-delek@users.noreply.github.com>
2023-07-11 18:37:18 +0530

    Create oops.py

---
+++ oops.py
@@ -0,0 +1,11 @@
+class Bike:
+    no_of_wheel=2
+    no_of_engine=1
+    def __init__(self,Brand,Color,Model,Mileage,CC):
+        self.brand=Brand
+        self.color=Color
+        self.model=Model
+        self.mileage=Mileage
+        self.cc=CC
+b1=Bike('Royal Enfield','Olive Green','Himalayan',33.3,411)
+b2=Bike('Royal Enfield','Carbon black','Meteor',36.2,349)
